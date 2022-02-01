5e37f482ca3a3ffd021585668026b4b2a036804a
Rishita Reddy Chilla <c.rishita.reddy@gmail.com>
2022-02-01 20:26:00 +0530

    Express Serve Static Files

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
+
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname+'/public')); //folder for which static html files will be served up
 
 app.use((req, res, next ) => {
-    console.log(req.headers);
+   // console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type' , 'text/html');
     res.end('<html><body><h1> This is an Express Server </h1></body></html>');
