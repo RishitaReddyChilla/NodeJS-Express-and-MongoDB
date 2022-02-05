f8fef05f088fd293e02cbe4e3e2430b4c7bec880
Rishita Reddy Chilla <c.rishita.reddy@gmail.com>
2022-02-05 22:04:38 +0530

    Mongoose Part 3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -11,24 +11,38 @@ connect.then((db)=>{
     //create and save a new dish
     Dishes.create({
         name: 'Uthappizza',
-        description: 'test'
+        description: 'test',
     })
     .then((dish) => {
         console.log(dish);
 
-        return Dishes.find({});
+        return Dishes.findByIdAndUpdate(dish._id,{
+            $set: { description: 'Updated test'}
+        },{
+            new:  true
+        }).exec();
     })
     //printing the dish
-    .then((dishes) => {
-        console.log(dishes);
+    .then((dish) => {
+        console.log(dish);
 
-        return Dishes.deleteOne({});
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+
+        });
+
+        return dish.save();
     })
+    .then((dish)=> {
+        console.log(dish);
+        return Dishes.deleteOne({});
     //closing the connection
+    })
     .then(()=>{
         return mongoose.connection.close();
     })
-
     .catch((err) => {
         console.log(err);
     });
