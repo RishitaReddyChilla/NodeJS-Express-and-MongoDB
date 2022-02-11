b47274770c6fa1d6a1b6c459c604c44bdc481777
Rishita Reddy Chilla <c.rishita.reddy@gmail.com>
2022-02-11 20:17:04 +0530

    Express REST API with MongoDB and Mongoose 2

--- conFusionServer/routes/dishRouter.js
+++ conFusionServer/routes/dishRouter.js
@@ -81,7 +81,158 @@ dishRouter.route('/:dishId')
       },(err)=> next(err))
       .catch((err) => next(err));
   });
+  
+  dishRouter.route('/:dishId/comments')
+// if modified res or req object above - that modified object is used below
+.get((req,res,next) =>{
+    Dishes.findById(req.params.dishId)
+    .then((dish)=>{
+      if(dish != null) {
+        res.statusCode=200;
+        res.setHeader('Content-Type','application/json');
+        res.json(dish.comments);
+      }
+      else{
+        err = new Error('Dish '+ req.params.dishId + ' not found');
+        err.status = 404;
+        return next(err);//app.js will handle error
+      }
+    }, (err)=> next(err))
+    .catch((err) => next(err));
+})
+.post((req,res,next) => {
+    Dishes.findById(req.params.dishId)
+    .then((dish)=>{
+      if(dish != null) {
+        dish.comments.push(req.body);
+        dish.save()
+        .then((dish)=> {
+          res.statusCode=200;
+          res.setHeader('Content-Type','application/json');
+          res.json(dish);
+        }, (err) => next(err));
+      }
+      else{
+        err = new Error('Dish '+ req.params.dishId + ' not found');
+        err.status = 404;
+        return next(err);//app.js will handle error
+      }
 
+    },(err)=> next(err))
+    .catch((err) => next(err));
+})
+.put( (req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /dishes/' + req.params.dishId + '/comments');
+  })
+.delete((req, res, next) => {
+  Dishes.findById(req.params.dishId)
+  .then((dish)=>{
+    if(dish != null) {
+      for(var i  = (dish.comments.length-1) ; i>=0 ;i--)
+      {
+        dish.comments.id(dish.comments[i]._id).remove();
+      }
+      dish.save()
+      .then((dish)=> {
+        res.statusCode=200;
+        res.setHeader('Content-Type','application/json');
+        res.json(dish);
+      }, (err) => next(err));
+    }
+    else{
+      err = new Error('Dish '+ req.params.dishId + ' not found');
+      err.status = 404;
+      return next(err);//app.js will handle error
+    }
+  },(err)=> next(err))
+  .catch((err) => next(err));
+});
+//1st group ends here 
+
+dishRouter.route('/:dishId/comments/:commentId')
+.get( (req,res,next) => {
+     Dishes.findById(req.params.dishId)
+     .then((dish)=>{
+      if(dish != null && dish.comments.id(req.params.commentId)!=null) {
+        res.statusCode=200;
+        res.setHeader('Content-Type','application/json');
+        res.json(dish.comments.id(req.params.commentId));
+      }
+      else if(dish == null){
+        err = new Error('Dish '+ req.params.dishId + ' not found');
+        err.status = 404;
+        return next(err);//app.js will handle error
+      }
+      else {
+        err = new Error('Comments '+ req.params.commentId + ' not found');
+        err.status = 404;
+        return next(err);
+      }
+    },(err)=> next(err))
+    .catch((err) => next(err));
+  })
+.post( (req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /dishes/'+ req.params.dishId +
+    '/comments/'+req.params.commentId);
+  })
+.put( (req, res, next) => {
+  Dishes.findById(req.params.dishId)
+  .then((dish)=>{
+   if(dish != null && dish.comments.id(req.params.commentId)!=null) {
+    if(req.body.rating) {
+      dish.comments.id(req.params.commentId).rating = req.body.rating;
+    }
+    if(req.body.comment){
+      dish.comments.id(req.params.commentId).comment = req.body.comment;
+    }
+    dish.save()
+    .then((dish)=> {
+      res.statusCode=200;
+      res.setHeader('Content-Type','application/json');
+      res.json(dish);
+    }, (err) => next(err));
+   }
+   else if(dish == null){
+     err = new Error('Dish '+ req.params.dishId + ' not found');
+     err.status = 404;
+     return next(err);//app.js will handle error
+   }
+   else {
+     err = new Error('Comments '+ req.params.commentId + ' not found');
+     err.status = 404;
+     return next(err);
+   }
+ },(err)=> next(err))
+    .catch((err) => next(err));
+  })
+
+.delete((req, res, next) => {
+  Dishes.findById(req.params.dishId)
+  .then((dish)=>{
+    if(dish != null && dish.comments.id(req.params.commentId)!=null) {
+      dish.comments.id(req.params.commentId).remove();
+      dish.save()
+      .then((dish)=> {
+        res.statusCode=200;
+        res.setHeader('Content-Type','application/json');
+        res.json(dish);
+      }, (err) => next(err));
+    }
+    else if(dish == null){
+      err = new Error('Dish '+ req.params.dishId + ' not found');
+      err.status = 404;
+      return next(err);//app.js will handle error
+    }
+    else {
+      err = new Error('Comments '+ req.params.commentId + ' not found');
+      err.status = 404;
+      return next(err);
+    }
+  },(err)=> next(err))
+      .catch((err) => next(err));
+  });
 /*
 //for all the requests - GET,PUT,POST,DELETE
 //1st app.all will be executed and later rest will be executed due to next() 
