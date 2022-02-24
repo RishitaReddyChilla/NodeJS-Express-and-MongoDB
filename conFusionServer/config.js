02ee7b1f87d45947d2e40d758b86c488fa9006b5
Rishita Reddy Chilla <c.rishita.reddy@gmail.com>
2022-02-24 21:26:54 +0530

    *OAuth edit

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey':'12345-67890-09876-54321',
     'mongoUrl': 'mongodb://localhost:27017/conFusion',
     'facebook':{
-        clientId: '618783329190008',
-        clientSecret: '52a31645e7919c1f943b4b9ae3298bbb'
+        clientId: '111111111111111',
+        clientSecret: '1111111111111111111111111'
     }
 }
